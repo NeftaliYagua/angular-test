c3995e02f6bf67156593a3aaa5d53906c46d92d3
Neftali Yagua <despacho@neftaliyagua.com>
2021-10-10 22:05:19 -0500

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactosComponent } from './crud/contactos/contactos.component';
+import { DireccionsComponent } from './crud/direccions/direccions.component';
+import { PedidosComponent } from './crud/pedidos/pedidos.component';
+import { ProductosComponent } from './crud/productos/productos.component';
+import { ContactoComponent } from './crud/contacto/contacto.component';
+import { DireccionComponent } from './crud/direccion/direccion.component';
+import { PedidoComponent } from './crud/pedido/pedido.component';
+import { ProductoComponent } from './crud/producto/producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {  path: '', redirectTo: 'home', pathMatch: 'full' },
+  {  path: 'home', loadChildren: './home/home.module#HomePageModule' },
+
+  {  path: 'contactos', component: ContactosComponent },
+  {  path: 'direccions', component: DireccionsComponent },
+  {  path: 'pedidos', component: PedidosComponent },
+  {  path: 'productos', component: ProductosComponent },
+
+  {  path: 'contacto/:id', component: ContactoComponent },
+  {  path: 'direccion/:id', component: DireccionComponent },
+  {  path: 'pedido/:id', component: PedidoComponent },
+  {  path: 'producto/:id', component: ProductoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatSliderModule } from '@angular/material/slider';
+
+import { ContactosComponent } from './crud/contactos/contactos.component';
+import { ContactoComponent } from './crud/contacto/contacto.component';
+import { DireccionesComponent } from './crud/direcciones/direcciones.component';
+import { DireccionComponent } from './crud/direccion/direccion.component';
+import { PedidosComponent } from './crud/pedidos/pedidos.component';
+import { PedidoComponent } from './crud/pedido/pedido.component';
+import { ProductosComponent } from './crud/productos/productos.component';
+import { ProductoComponent } from './crud/producto/producto.component';
+import { DireccionsComponent } from './crud/direccions/direccions.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactosComponent,
+    ContactoComponent,
+    DireccionesComponent,
+    DireccionComponent,
+    PedidosComponent,
+    PedidoComponent,
+    ProductosComponent,
+    ProductoComponent,
+    DireccionsComponent
   ],
   imports: [
+    MatSliderModule,
     BrowserModule,
     AppRoutingModule
   ],
